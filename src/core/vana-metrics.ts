2ed58c9defbed60ea66e2d680457582ed6728ddf
Max <max@vana.gt>
2022-08-23 12:10:06 -0600

    feat(sdk): Add initial sdk version, for logging and metrics wit datadog

---
+++ src/core/vana-metrics.ts
@@ -0,0 +1,50 @@
+import VanaLogger from "./vana-logger";
+import {sendDistributionMetric} from "datadog-lambda-js";
+const logger = new VanaLogger();
+
+const metrics = {
+    count: "c",
+    gauge: "g",
+    rate: "r",
+    set: "s",
+    histogram: "h",
+    distribution: "d",
+};
+
+class VanaMetrics {
+    metric(name: string, value: number, type: string, ...tags: string[]): void {
+        try {
+            sendDistributionMetric(name, value, ...tags);
+        } catch (ex) {
+            logger.warning("Import error, lambda_metric datadog not available. Using normal logger to send metric.");
+            logger.setTags(tags);
+            logger.info(`${name}:${value}|${type}`);
+        }
+    }
+
+    count(name: string, value: number, ...tags: string[]): void {
+        return this.metric(name, value, metrics.count, ...tags)
+    }
+
+    gauge(name: string, value: number, ...tags: string[]): void {
+        return this.metric(name, value, metrics.gauge, ...tags)
+    }
+
+    rate(name: string, value: number, ...tags: string[]): void {
+        return this.metric(name, value, metrics.rate, ...tags)
+    }
+
+    histogram(name: string, value: number, ...tags: string[]): void {
+        return this.metric(name, value, metrics.histogram, ...tags)
+    }
+
+    set(name: string, value: number, ...tags: string[]): void {
+        return this.metric(name, value, metrics.set, ...tags)
+    }
+
+    distribution(name: string, value: number, ...tags: string[]): void {
+        return this.metric(name, value, metrics.distribution, ...tags)
+    }
+}
+
+export default VanaMetrics;
