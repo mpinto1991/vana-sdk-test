3137c4a8eabe83ed708f1b2108ea8b5ea63a9ce2
Max <max@vana.gt>
2022-08-29 15:16:26 -0600

    feat: Move vana logger to infrastrucure/util folder. Add use case utilities and validation error

---
+++ src/infrastructure/util/use-case-utilities.ts
@@ -0,0 +1,28 @@
+import { ValidationError } from "../errors/validation-error";
+import { VanaLogger } from "../util/vana-logger"
+import { VanaEvent } from "../types/VanaEventBase";
+
+let logger = new VanaLogger();
+
+export class UseCaseUtilities {
+    static extractData(event: VanaEvent) {
+        let data;
+        try {
+            if (event["body"]) {
+                const body = JSON.parse(event.body);
+                data = body["data"];
+            } else {
+                data = event["data"];
+            }
+        } catch (error) {
+            let err = error as Error;
+            logger.error(err);
+            throw new ValidationError("Could not parse data: " + err.message);
+        }
+        if (!data) {
+            logger.error("Validation error: Missing data property");
+            throw new ValidationError("Validation error: Missing data property");
+        }
+        return data;
+    }
+}
